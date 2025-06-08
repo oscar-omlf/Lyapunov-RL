CEGAR fixes and fixes to numerical precission in forward with grad.

src/make_plots.py
@@ -105,25 +105,6 @@ def plot_lyapunov_3d(
 device_name = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f"Using device: {device_name}")
 
-
-config_lac = {
-        "agent_str": "Lyapunov-AC",
-        "alpha": 0.1,
-        "lr": 3e-3,
-        "dynamics_fn": vanderpol_dynamics_torch,
-        "batch_size": 64,
-        "num_paths_sampled": 8,
-        "dt": 0.01,
-        "norm_threshold": 5e-2,
-        "integ_threshold": 50,
-        "r1_bounds": (np.array([-2.0, -2.0]), np.array([2.0, 2.0])),
-        "actor_hidden_sizes": (30, 30),
-        "critic_hidden_sizes": (30, 30),
-        "state_space": np.zeros(2),
-        "action_space":np.zeros(1),
-        "max_action": MAX_ACTION
-}
-
 config_lac_pendulum = {
     "agent_str": "Lyapunov-AC",
     "model_name": "LAC",
@@ -147,7 +128,7 @@ config_lac_pendulum = {
 print("Initializing Lyapunov-AC Agent...")
 lac_agent = LyapunovAgent(config=config_lac_pendulum)
 
-lac_agent.load(file_path='./logs/LAC/run_1/', episode=3000)
+lac_agent.load(file_path='./logs/LAC/run_2/', episode=3000) 
 print("Lyapunov-AC Agent loaded successfully.")
 
 config_lqr = {
@@ -240,13 +221,13 @@ plot_streamlines(ax_2d, X_flow_np, Y_flow_np,
 
 # Contour for LQR V(x)
 alpha = 0.2
-lqr_contour_val = 0.430  # TODO: Tune this!
+lqr_contour_val = 0.4230  # TODO: Tune this!
 lqr_contour_val = math.tanh(lqr_contour_val) / alpha
 print(f"LQR certified c* = {lqr_contour_val:.4f}")
 ax_2d.contour(X_grid_np, Y_grid_np, V_lqr_grid_np, levels=[lqr_contour_val], linewidths=2, colors='magenta', linestyles='--')
 
 # Contour for LyAC W(x)
-lyac_contour_val = 0.0241
+lyac_contour_val = 0.0779
 ax_2d.contour(X_grid_np, Y_grid_np, W_grid_np, levels=[lyac_contour_val],linewidths=2, colors='red')
 
 # Plot Styling

src/models/lyapunov_critic.py
@@ -20,9 +20,16 @@ class LyapunovCritic(nn.Module):
         return self.model(x).squeeze(-1)
     
     def forward_with_grad(self, x):
+        x.requires_grad_(True)        
         y = self(x)
-        jacob = torch.autograd.functional.jacobian(self, (x,), create_graph=True)[0]
-        grad = torch.diagonal(jacob, dim1=0, dim2=1).T
+        
+        grad = torch.autograd.grad(
+            outputs=y.sum(), 
+            inputs=x, 
+            create_graph=True
+        )[0]
+        
+        x.requires_grad_(False)
         return y, grad
 
     def forward_dreal(self, x_vars):

src/trainers/lyapunov_trainer.py
@@ -331,7 +331,7 @@ class LyapunovTrainer(Trainer):
         W0 = self.critic_model(torch.zeros((1, self.state_dim), device=self.device)).squeeze().item()
         x = dreal_var(self.state_dim)
 
-        # --- Construct dReal expressions ---
+        # Construct dReal expressions
         u = self.actor_model.forward_dreal(x)
         fx = self.dynamics_fn_dreal(x, u)
         Wx = self.critic_model.forward_dreal(x)[0]
@@ -349,7 +349,7 @@ class LyapunovTrainer(Trainer):
                 Wx <= W0
             )
         )
-        r1 = d.CheckSatisfiability(condition1, 0.01)
+        r1 = d.CheckSatisfiability(condition1, 0.001)
         if r1:
             print("Verification failed on Condition 1 (Lie Deriv or V(x)<=V(0) violated).")
             return (False, r1)
@@ -359,10 +359,10 @@ class LyapunovTrainer(Trainer):
             self.on_boundry_dreal(x, scale=scale),
             Wx <= level
         )
-        r2 = d.CheckSatisfiability(condition2, 0.01)
+        r2 = d.CheckSatisfiability(condition2, 0.001)
         if r2:
             print("Verification failed on Condition 2 (RoA touches boundary).")
             return (False, r2)
 
-        print("--- Verification PASSED for this level ---")
+        print("Verification PASSED for this level")
         return (True, None)

src/util/dynamics.py
@@ -153,29 +153,28 @@ def double_integrator_dynamics_dreal(state_vars, action_vars):
 
 
 def vanderpol_dynamics_torch(state: torch.Tensor, action: torch.Tensor, mu: float = 1.0) -> torch.Tensor:
-    if state.ndim == 1:  # Single sample
+    if state.ndim == 1:
         x1, x2 = state[0], state[1]
         u = action[0]
-        
         x1_dot = x2
-        x2_dot = x1 - mu * (1 - x1**2) * x2 + u
+        x2_dot = -x1 + mu * (1 - x1**2) * x2 + u
         return torch.stack([x1_dot, x2_dot])
-    elif state.ndim == 2:  # Batch of samples
+    
+    elif state.ndim == 2:
         x1 = state[:, 0]
         x2 = state[:, 1]
-        u = action[:, 0] # Assuming action is (N,1) so u becomes (N,)
-        
+        u = action[:, 0]        
         x1_dot = x2
-        x2_dot = x1 - mu * (1 - x1**2) * x2 + u
+        x2_dot = -x1 + mu * (1 - x1**2) * x2 + u
         return torch.stack([x1_dot, x2_dot], dim=1)
 
 
 def vanderpol_dynamics_np(state: np.ndarray, action: np.ndarray, mu: float = 1.0) -> np.ndarray:
-    if state.ndim == 1: # Single sample
+    if state.ndim == 1:
         state = state.reshape(1, -1)
-    if action.ndim == 0: # Single scalar action
+    if action.ndim == 0:
         action = np.array([action]).reshape(1, -1)
-    elif action.ndim == 1 and action.shape[0] == state.shape[0]: # Batch of scalar actions
+    elif action.ndim == 1 and action.shape[0] == state.shape[0]:
         action = action.reshape(-1,1)
 
 